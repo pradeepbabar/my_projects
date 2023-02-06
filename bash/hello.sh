f1983b00f0079fd752b9c3d054aa64326f3f9a45
Pradeep Babar <pradeepbabar31@gmail.com>
2023-02-07 00:37:57 +0530

    added some line in hello shell scipt

--- bash/hello.sh
+++ bash/hello.sh
@@ -11,5 +11,24 @@ echo $PWD
 name=MArk
 echo The name is $name
 
+#Read User Input
 
-# Read user input
+echo "Enter Names: "
+read name1 name2 name3 #whenever you need input from terminal use read command
+echo "Entered name: $name1, $name2, $name3"
+
+#Read User Input on same line
+read -p 'username:' user_var #-p userinput for sameline
+echo "username : $user_var"
+read -sp 'password:' pass_var #-sp userinput for silent for password
+echo "password : $pass_var"
+
+#allow thhe user for multiple input and read an array
+echo "Enter names:"
+read -a names
+echo "Names: ${names[0]}, ${names[1]}"
+
+#built in variable called replay
+echo "Enter name:"
+read
+echo "Name : $REPLAY"
